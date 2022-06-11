26cac3ee65444c33192df783c41ba4911be7072b
kashin777 <kashin777@gmail.com>
2022-06-12 03:16:29 +0900

    コメント追加＆リファクタリング

--- Pages/ValidationPropertyError.xaml.cs
+++ Pages/ValidationPropertyError.xaml.cs
@@ -1,57 +1,59 @@
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 namespace MauiAppDemo.Pages;
 
 public partial class ValidationPropertyError : ContentView
 {
-    public static readonly BindableProperty PageViewModelProperty = BindableProperty.Create("PageViewModel", typeof(PageViewModel), typeof(ValidationPropertyError), propertyChanged: PageViewModel_PropertyChanged);
+    public static readonly BindableProperty TargetModelProperty = 
+        BindableProperty.Create("TargetModel", typeof(ValidationPropertyModel), typeof(ValidationPropertyError), propertyChanged: TargetModelProperty_PropertyChanged);
 
-    public PageViewModel PageViewModel { 
-        set => SetValue(PageViewModelProperty, value);
-        get => GetValue(PageViewModelProperty) as PageViewModel;
+    public ValidationPropertyModel TargetModel
+    {
+        set => SetValue(TargetModelProperty, value);
+        get => GetValue(TargetModelProperty) as ValidationPropertyModel;
     }
 
-    public string Property { 
-        get; 
-        set; 
+    public string PropertyName
+    {
+        get;
+        set;
     }
 
-	public ValidationPropertyError()
-	{
-		InitializeComponent();
-	}
+    public ValidationPropertyError()
+    {
+        InitializeComponent();
+    }
 
-    static void PageViewModel_PropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    static void TargetModelProperty_PropertyChanged(BindableObject bindable, object oldValue, object newValue)
     {
         var errorView = (ValidationPropertyError)bindable;
 
-        if(oldValue != null)
+        if (oldValue != null)
         {
-            errorView.PageViewModel.PropertyChanged -= errorView.PageViewModel_PropertyChanged;
+            errorView.TargetModel.PropertyChanged -= errorView.TargetModel_PropertyChanged;
         }
 
         if (newValue != null)
         {
-            errorView.PageViewModel.PropertyChanged += errorView.PageViewModel_PropertyChanged;
+            errorView.TargetModel.PropertyChanged += errorView.TargetModel_PropertyChanged;
         }
     }
 
-    private void PageViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    private void TargetModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
     {
         if (e.PropertyName.Equals("Errors"))
         {
             var errorViewModel = BindingContext as ValidationPropertyErrorViewModel;
             errorViewModel.ClearErrors();
-            foreach (var error in PageViewModel.PropertyErrors(Property))
+            foreach (var error in TargetModel.PropertyErrors(PropertyName))
             {
-                errorViewModel.AddError(Property, error);
+                errorViewModel.AddError(PropertyName, error);
             }
         }
     }
 }
 
-public class ValidationPropertyErrorViewModel : PageViewModel
+public class ValidationPropertyErrorViewModel : ValidationPropertyModel
 {
 
 }
--- Pages/ValidationPropertyErrors.xaml.cs
+++ Pages/ValidationPropertyErrors.xaml.cs
@@ -1,9 +1,53 @@
+using System.ComponentModel;
+
 namespace MauiAppDemo.Pages;
 
 public partial class ValidationPropertyErrors : ContentView
 {
-	public ValidationPropertyErrors()
+    public static readonly BindableProperty TargetModelProperty = 
+        BindableProperty.Create("TargetModel", typeof(ValidationPropertyModel), typeof(ValidationPropertyErrors), propertyChanged: TargetModelProperty_PropertyChanged);
+
+    public ValidationPropertyModel TargetModel
+    {
+        set => SetValue(TargetModelProperty, value);
+        get => GetValue(TargetModelProperty) as ValidationPropertyModel;
+    }
+
+    public ValidationPropertyErrors()
 	{
 		InitializeComponent();
 	}
+
+    static void TargetModelProperty_PropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var errorView = (ValidationPropertyErrors)bindable;
+
+        if (oldValue != null)
+        {
+            errorView.TargetModel.PropertyChanged -= errorView.TargetModel_PropertyChanged;
+        }
+
+        if (newValue != null)
+        {
+            errorView.TargetModel.PropertyChanged += errorView.TargetModel_PropertyChanged;
+        }
+    }
+
+    private void TargetModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName.Equals("Errors"))
+        {
+            var errorViewModel = BindingContext as ValidationPropertyErrorsViewModel;
+            errorViewModel.ClearErrors();
+            foreach (var error in TargetModel.Errors)
+            {
+                errorViewModel.AddError(ValidationPropertyModel.MODEL_ERROR, error);
+            }
+        }
+    }
+}
+
+public class ValidationPropertyErrorsViewModel : ValidationPropertyModel
+{
+
 }
